831bab34ed912497abd4e943b65157248ab4b086
Alejandro Salas <asalas.wd@outlook.com>
2022-11-22 21:35:13 -0500

    Create all elements for calculator

--- src/com/java21days/BasicCalculator.java
+++ src/com/java21days/BasicCalculator.java
@@ -4,9 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
-public class BasicCalculator extends JFrame implements ActionListener {
-    Dimension dim = new Dimension(500, 200);
+public class BasicCalculator extends JFrame implements ActionListener, FocusListener {
+    Dimension dim = new Dimension(100, 150);
     Float result;
     JLabel resultLabel;
 
@@ -14,7 +16,18 @@ public class BasicCalculator extends JFrame implements ActionListener {
         super("Basic Calculator");
         setSize(dim);
         setLocationRelativeTo(null);
-
+        JPanel panel = new JPanel();
+        JTextField num1 = new JTextField("0", 6);
+        JTextField num2 = new JTextField("0", 6);
+        panel.add(num1);
+        panel.add(num2);
+        JButton add = new JButton("+");
+        JButton subtract = new JButton("-");
+        panel.add(add);
+        panel.add(subtract);
+        resultLabel = new JLabel("Result here");
+        panel.add(resultLabel);
+        add(panel);
         setVisible(true);
     }
 
@@ -24,5 +37,15 @@ public class BasicCalculator extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         System.out.println("Action performed");
+    }
+
+    @Override
+    public void focusGained(FocusEvent focusEvent) {
+
+    }
+
+    @Override
+    public void focusLost(FocusEvent focusEvent) {
+
     }
 }
